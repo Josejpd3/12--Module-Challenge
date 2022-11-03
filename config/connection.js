963c07dd5c13badea010951aec23366ccdfe2855
Jose Plasencia <josejpd3@gmail.com>
2022-11-03 19:14:16 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,4 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(
+const dbKeys = new Keys(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -12,4 +9,4 @@ const sequelize = new Sequelize(
     }
   );
   
-  module.exports = sequelize;
+  module.exports = dbKeys;

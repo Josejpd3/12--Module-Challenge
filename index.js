f894f90a12f8295749bea1ffe9702e3f2d3aca8d
Jose Plasencia <josejpd3@gmail.com>
2022-11-03 21:45:47 -0400

    initQuestion prompt

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const inquirer = require('inquirer');
 const db = require('./config/connection')
+
+const initQuestion = function () {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "start",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee",
+                "Quit"
+            ],
+        }
+    ])
+}
+
+initQuestion()

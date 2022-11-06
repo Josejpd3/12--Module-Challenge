07644413ba83a9fa1365f9bf68870d0bf5f63448
Jose Plasencia <josejpd3@gmail.com>
2022-11-06 16:15:53 -0500

    insert data in employee table

--- db/seed.sql
+++ db/seed.sql
@@ -13,3 +13,10 @@ VALUES
 ('Career Services', 40000, 2),
 ('Mobile Designer', 80000, 3),
 ('Sales Associate', 50000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES 
+('Frank', 'Davila', 1),
+('Ryker', 'Lucas', 2),
+('Reese', 'Talbot', 3),
+('Ross', 'Rosario', 4);

159d223ca49888dcd4eb72fb314d4c3fb99729ea
Jose Plasencia <josejpd3@gmail.com>
2022-11-06 16:15:23 -0500

    insert data in role table

--- db/seed.sql
+++ db/seed.sql
@@ -6,3 +6,10 @@ VALUES
 ('Finance'),
 ('Design'),
 ('Service');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Senior Engineer', 55000, 1),
+('Career Services', 40000, 2),
+('Mobile Designer', 80000, 3),
+('Sales Associate', 50000, 4);
